b0d7f3f1522f6e9f7d3fd6d40685defe783c6877
Michael Martinez <mirima92@gmail.com>
2026-07-21 19:52:50 -0400

    add motion to stats section

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -5,17 +5,17 @@ import * as V from '../motion/index'
 export default function Stats() {
   return (<>
     <section className="py-14">
-      <div className="container grid grid-cols-2 lg:grid-cols-4 gap-4">
+      <motion.div variants={V.staggerContainer} initial="hidden" whileInView={"show"} viewport={{ once:true }} className="container grid grid-cols-2 lg:grid-cols-4 gap-4">
         {statsItems.map(({ value, label, color }) => (
           // CARD
-          <div key={label} className="hero-card p-6 text-center rounded-2xl">
+          <motion.div variants={V.scale} key={label} className="hero-card p-6 text-center rounded-2xl">
             {/* TITLE */}
             <div className="text-4xl font-bold mb-1" style={{ color }}>{value}</div>
             {/* TEXT */}
             <p className="text-md">{label}</p>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
   </>);
 }
