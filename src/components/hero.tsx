6154a87e5ffe17f83fe1872cb4b90fc629e5647c
Michael Martinez <mirima92@gmail.com>
2026-07-21 12:18:04 -0400

    add motion to hero section; add accessibility changes to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,56 +1,57 @@
 import { ArrowRight, CheckCircle2, Sparkles, Zap } from "lucide-react";
-import React from "react";
+import { motion } from "motion/react";
+import * as V from "../motion/index";
 
 export default function Hero() {
   return (<>
     <section className="py-20">
-      <div className="container grid gap-11 lg:grid-cols-[1fr_0.7fr] lg:items-center">
+      <motion.div variants={V.staggerContainer} initial="hidden" whileInView={"show"} viewport={{ once:true }} className="container grid gap-11 lg:grid-cols-[1fr_0.7fr] lg:items-center">
         {/* CONTENT */}
         <div className="text-center lg:text-left">
           
           {/* BADGE */}
-          <div className="section-tag">
+          <motion.div variants={V.fadeIn} className="section-tag">
             <span>Limited Time - 20% Off</span>
-          </div>
+          </motion.div>
           
           {/* TITLE */}
-          <h1 className="text-5xl sm:text-6xl lg:text-7xl font-black leading-tight tracking-tight mb-6">
+          <motion.h1 variants={V.fadeIn} className="text-5xl sm:text-6xl lg:text-7xl font-black leading-tight tracking-tight mb-6">
             Learn anything. <span className="bg-pink text-white inline-block px-2 rounded-lg -rotate-1 border-2 border-border">Faster.</span>
-          </h1>
-          <p className="text-lg mb-8 nax-w-lg mx-auto lg:mx-0 leading-relaxed">FlashMaster uses spaced repetition to help you memorize anything vocabulary, code, facts, formulas in less time with better results.</p>
+          </motion.h1>
+          <motion.p variants={V.fadeIn} className="text-lg mb-8 nax-w-lg mx-auto lg:mx-0 leading-relaxed">FlashMaster uses spaced repetition to help you memorize anything vocabulary, code, facts, formulas in less time with better results.</motion.p>
           
           {/* BUTTONS WRAPPER */}
           <div className="flex flex-wrap gap-3 justify-center lg:justify-start mb-8">
-            <a href="#" className="btn-primary">
+            <motion.a variants={V.fadeIn} href="#" className="btn-primary">
               Start for Free
               <ArrowRight size={20} />
-            </a>
-            <a href="#" className="btn-secondary">
+            </motion.a>
+            <motion.a variants={V.fadeIn} href="#" className="btn-secondary">
               See How It Works
-            </a>
+            </motion.a>
           </div>
 
           {/* SOCIAL PROOF PILLS */}
           <div className="flex flex-wrap gap-2 justify-center lg:justify-start">
-            <span className="stat-pill">
+            <motion.span variants={V.fadeIn} className="stat-pill">
               <CheckCircle2 className="size-6 text-teal-stat" />
               No password required
-            </span>
-            <span className="stat-pill">
+            </motion.span>
+            <motion.span variants={V.fadeIn} className="stat-pill">
               <Sparkles className="size-6 text-yellow" />
               Magic link sign-in
-            </span>
-            <span className="stat-pill">
+            </motion.span>
+            <motion.span variants={V.fadeIn} className="stat-pill">
               <Zap className="size-6 text-pink-stat" />
               Spaced Repetition
-            </span>
+            </motion.span>
           </div>
         </div>
         {/* CARDS */}
         <div className="flex-1 relative flex items-center justify-center min-h-95">
           {/* BACK */}
-          <div className="float3 absolute fake-card bg-border max-w-65 w-full p-7 top-5 right-2 rotate-3 z-1">
-            <div className="text-xs font-bold mb-3 px-2 py-1 rounded-full inline-block bg-white/10 text-white">
+          <motion.div variants={V.scale} className="float3 absolute fake-card bg-border max-w-65 w-full p-7 top-5 right-2 rotate-3 z-1">
+            <div className="text-xs font-bold mb-3 px-2 py-1 rounded-full inline-block bg-gray-100 border border-1 text-gray-900">
               JavaScript
             </div>
             <p className="text-base font-black text-white leading-snug tracking-wide">
@@ -65,11 +66,11 @@ export default function Hero() {
                 />
               ))}
             </div>
-          </div>
+          </motion.div>
           
           {/* MIDDLE */}
-          <div className="absolute fake-card bg-blue-stat max-w-65 w-full p-7 top-10 left-2 -rotate-2 z-1 ">
-            <div className="text-xs font-bold mb-3 px-2 py-1 rounded-full inline-block bg-white/10 text-white">
+          <motion.div variants={V.scale} className="absolute fake-card max-w-65 w-full p-7 top-10 left-2 -rotate-2 z-1" style={{backgroundColor:"#4D6ED1"}}>
+            <div className="text-xs font-bold mb-3 px-2 py-1 rounded-full inline-block bg-gray-100 border border-1 text-gray-900">
               Biology
             </div>
             <p className="text-base font-black text-white leading-snug tracking-wide">
@@ -84,11 +85,11 @@ export default function Hero() {
                 />
               ))}
             </div>
-          </div>
+          </motion.div>
           
           {/* FRONT */}
-          <div className="relative fake-card bg-pink-600 max-w-65 w-full p-7 z-3">
-            <div className="text-xs font-bold mb-3 px-2 py-1 rounded-full inline-block bg-white/10 text-white">
+          <motion.div variants={V.scale} className="relative fake-card bg-pink-600 max-w-65 w-full p-7 z-3">
+            <div className="text-xs font-bold mb-3 px-2 py-1 rounded-full inline-block bg-gray-100 border border-1 text-gray-900">
               Web Development
             </div>
             <p className="text-base font-black text-white leading-snug tracking-wide">
@@ -103,10 +104,10 @@ export default function Hero() {
                 />
               ))}
             </div>
-          </div>
+          </motion.div>
 
         </div>
-      </div>
+      </motion.div>
     </section>
   </>)
 }
