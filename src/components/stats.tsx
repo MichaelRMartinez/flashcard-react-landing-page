70872fd314233849acfe7098b1cecd5be4e861e4
Michael Martinez <mirima92@gmail.com>
2026-07-20 22:41:11 -0400

    complete stats section

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { statsItems } from "../constant/data";
 
 export default function Stats() {
-  return <div>Stats</div>;
+  return (<>
+    <section className="py-14">
+      <div className="container grid grid-cols-2 lg:grid-cols-4 gap-4">
+        {statsItems.map(({ value, label, color }) => (
+          // CARD
+          <div key={label} className="hero-card p-6 text-center rounded-2xl">
+            {/* TITLE */}
+            <div className="text-4xl font-bold mb-1" style={{ color }}>{value}</div>
+            {/* TEXT */}
+            <p className="text-md">{label}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  </>);
 }
