d18c1d9dbc061b39821cc2e95c6b6f1dc55a3b4d
Michael Martinez <mirima92@gmail.com>
2026-07-21 11:38:58 -0400

    complete footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { BookOpen } from "lucide-react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (<>
+  <footer className="py-8 border-t border-neutral-600">
+    <div className="container flex flex-col sm:flex-row items-center justify-between gap-4">
+      {/* FOOTER LOGO */}
+      <div className="flex items-center gap-2">
+        <div className="nav-logo size-7">
+          <BookOpen size={20} />
+        </div>
+        <span className="font-bold tracking-tight">FlashMaster</span>
+      </div>
+      {/* COPYRIGHT INFO */}
+      <p>&copy; {new Date().getFullYear()}. All Rights Reserved</p>
+      {/* LINKS */}
+      <div className="flex gap-4">
+        <a href="#" className="underline hover:underline-offset-2 hover:text-blue-800">Sign In</a>
+        <a href="#" className="underline hover:underline-offset-2 hover:text-blue-800">Get Started</a>
+      </div>
+    </div>
+  </footer>
+  </>);
 }
